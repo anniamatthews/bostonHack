01e36159187cd40fe0602551338e31801bb354b5
Annia <98440326+anniamatthews@users.noreply.github.com>
2023-02-26 08:30:01 -0500

    added a comment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,69 @@
-import React from "react";
-
-const App = () => {
+import React, { useState } from 'react';
 
 
+/**
+ * This code is nowhere near completed. 
+ * I wanted to use this hackathon to get more familiar with React and Git, 
+ * so that was exactly what I did
+ * 
+ */
 
-    return(
-        <h1> App</h1>
-    )
+
+function Login(){
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+          const response = await fetch('/api/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, password }),
+          });
+          const data = await response.json();
+          console.log(data);
+          // handle response data
+        } catch (error) {
+          console.error(error);
+          // handle error
+        }
+      };
+
+
+
+    return (
+        <form onSubmit={handleSubmit}>
+          <label>
+            Username:
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <br />
+          <button type="submit">Login</button>
+        </form>
+      );
+
+
+
+    function newFunction() {
+        return useState('');
+    }
 }
 
-export default App; 
+
+
+
+export default login;
